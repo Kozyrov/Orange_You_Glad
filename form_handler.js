9f230f8447c13ed168853d15034d22d19dc3b7ed
Frankenfield <Kozyrov>
2017-08-26 19:12:45 -0700

    figured out a way to get the selected value from the radio menu using jQuery.

--- form_handler.js
+++ form_handler.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $(":button").click(function(){
-        console.log('submit button was pressed');
+        //this function will intake the user's selection
+        form_handler();
     });
 })
+
+function form_handler() {
+    //determine which answer was selected
+    console.log($("input[type='radio'][name='fruit_box']:checked").val());
+}
