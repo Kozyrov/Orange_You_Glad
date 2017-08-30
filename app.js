7a2ac09c9dd305a2d00f257a3777bdbdfcdf2942
Frankenfield <Kozyrov>
2017-08-29 17:49:48 -0700

    fixed the thing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 //getting the express app started
 var app = express();
