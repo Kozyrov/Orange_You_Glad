ca063235a86d8ffc8620c941a1b5a7183241a24c
Frankenfield <Kozyrov>
2017-08-30 16:20:21 -0700

    MVP version of the full app including user accessable front end.

--- public/js/form_handler.js
+++ public/js/form_handler.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    $(":radio").click(()=>{
+        let selected = $("input[type='radio'][name='fruit_box']:checked").val();
+        $(".selection").text("you've selected, " + selected)
+    })
     $(":button").click(function(){
         let email_regex = /^[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+$/
         let email = $("input[type='text'][name='user_email']").val();
@@ -13,10 +17,19 @@ $(document).ready(function(){
             win_condition(new User_result(email, final_answer));
         }        
     });
-})
+});
+
+function random_fruit(){
+    let random = Math.round(Math.random());
+    if (random===1){
+        $(".fruit_revealed").attr("src", "./../assets/images/apple.png");
+    } else {
+        $(".fruit_revealed").attr("src", "./../assets/images/orange.png");
+    }
+}
+
 function win_condition(current_user) {
     let correct_answer = "both"
-    console.log(current_user.answer);
     if (current_user.answer===correct_answer){
         current_user.correct=true;
         console.log("you've answered correctly");
